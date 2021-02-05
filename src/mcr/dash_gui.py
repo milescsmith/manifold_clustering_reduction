698ac29e245090ece519fc8c25601722c55be52e
Miles Smith <34606026+milescsmith@users.noreply.github.com>
2021-02-05 11:22:58 -0600

    Update dash_gui.py

--- src/mcr/dash_gui.py
+++ src/mcr/dash_gui.py
@@ -8,6 +8,7 @@ from typing import List
 from typing import Optional
 from typing import Tuple
 
+import click
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
